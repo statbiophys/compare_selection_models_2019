68d84a0f3dc70562d74754efc231711072f6b305
giulioisac <giulioisac@gmail.com>
2020-03-15 16:15:03 +0100

    comments

--- fig2_infer.py
+++ fig2_infer.py
@@ -11,13 +11,43 @@ import olga.load_model as olga_load_model
 import olga.generation_probability as generation_probability
 import olga.sequence_generation as seq_gen
 import os
-import process_adaptive as pa
 from scipy import stats
 import matplotlib.pyplot as plt
 import re
 import keras.backend as K
 import tensorflow as tf
 import random as rn
+
+## these functions come from the vampire package: preprocess_adaptive.py script
+translation_csv = 'sampled_data/adaptive-olga-translation.csv'
+def convert_gene_names(df, conversion_dict):
+    converted = df.copy()
+    converted['v_gene'] = df['v_gene'].map(conversion_dict['TRBV'].get)
+    converted['j_gene'] = df['j_gene'].map(conversion_dict['TRBJ'].get)
+    return converted
+
+def convert_and_filter(df, conversion_dict):
+    orig_len = len(df)
+    converted = convert_gene_names(filter_by_gene_names(df,conversion_dict), conversion_dict)
+    n_trimmed = orig_len - len(converted)
+    if n_trimmed > 0:
+        print("Warning: couldn't convert some sequences and trimmed them off, n=",n_trimmed)
+    return converted
+
+def adaptive2olga(adaptive_csv):
+    if isinstance(adaptive_csv, pd.DataFrame):df=adaptive_csv
+    else: df = pd.read_csv(adaptive_csv, usecols=['amino_acid', 'v_gene', 'j_gene'])
+    return convert_and_filter(df, adaptive_to_olga_dict())
+
+def olga2adaptive(olga_tsv):
+    if isinstance(olga_tsv, pd.DataFrame):df=olga_tsv
+    else: df = pd.read_csv(olga_tsv, usecols=['amino_acid', 'v_gene', 'j_gene'])
+    if len(df.columns) == 4:
+        df = df.iloc[:, 1:4]
+    assert len(df.columns) == 3
+    df.columns = ['amino_acid', 'v_gene', 'j_gene']
+
+    return convert_and_filter(df, olga_to_adaptive_dict())
 
 # set seeds for reproducibility
 np.random.seed(42)
@@ -30,9 +60,10 @@ K.set_session(sess)
     
 #load data
 data=pd.read_csv('sampled_data/deneuter_data.csv')
-data=list(pa.adaptive2olga(data).values)
+data=list(adaptive2olga(data).values)
 gen=list(pd.read_csv('sampled_data/generated_sequences.csv').values.astype(np.str)[:int(15e5)])
 print len(data),len(gen)
+
 # gen model
 main_folder='deneuter_model'
 params_file_name = os.path.join(main_folder,'model_params.txt')
@@ -46,15 +77,7 @@ generative_model = olga_load_model.GenerativeModelVDJ()
 generative_model.load_and_process_igor_model(marginals_file_name)
 pgen_model = seq_gen.SequenceGenerationVDJ(generative_model, genomic_data)
 
-#LENGTHPOS MODEL
-qm = SoniaLengthPos(data_seqs = data, gen_seqs=gen,custom_pgen_model='deneuter_model')
-qm.infer_selection(epochs=50,batch_size=int(1e4),validation_split=0.01)
-qm.save_model('selection_models/deneuter_model_lengthpos')
-ev=SequenceGeneration(sonia_model=qm,custom_olga_model=pgen_model,custom_genomic_data=genomic_data)
-seqs_sonia=ev.generate_sequences_post(int(2e4))
-pd.DataFrame(seqs_sonia,columns=['amino_acid','v_gene','j_gene']).to_csv('sampled_data/generated_sonia_lengthpos.csv',index=False)
-
-#LEFTRIGHT MODEL
+#LEFTRIGHT INFERENCE
 qm = SoniaLeftposRightpos(data_seqs = data, gen_seqs=gen,custom_pgen_model='deneuter_model')
 qm.infer_selection(epochs=50,batch_size=int(1e4),validation_split=0.01)
 qm.save_model('selection_models/deneuter_model_leftright')
@@ -62,15 +85,6 @@ ev=SequenceGeneration(sonia_model=qm,custom_olga_model=pgen_model,custom_genomic
 seqs_sonia=ev.generate_sequences_post(int(2e4))
 pd.DataFrame(seqs_sonia,columns=['amino_acid','v_gene','j_gene']).to_csv('sampled_data/generated_sonia_leftright.csv',index=False)
 
-# OLGA Q MODEL
-qm = SoniaVJL(data_seqs = data, gen_seqs=gen,custom_pgen_model='deneuter_model')
-qm.infer_selection(epochs=50,batch_size=int(1e4))
-qm.save_model('selection_models/deneuter_model_VJL')
-ev=SequenceGeneration(sonia_model=qm,custom_olga_model=pgen_model,custom_genomic_data=genomic_data)
-seqs_sonia=ev.generate_sequences_post(int(2e4))
-pd.DataFrame(seqs_sonia,columns=['amino_acid','v_gene','j_gene']).to_csv('sampled_data/generated_sonia_VJL.csv',index=False)
 
-# convert to adaptive format for evaluation
-pa.olga2adaptive(pd.read_csv('sampled_data/generated_sonia_lengthpos.csv')).to_csv('sampled_data/generated_sonia_lengthpos_adaptive.csv')
-pa.olga2adaptive(pd.read_csv('sampled_data/generated_sonia_leftright.csv')).to_csv('sampled_data/generated_sonia_leftright_adaptive.csv')
-pa.olga2adaptive(pd.read_csv('sampled_data/generated_sonia_VJL.csv')).to_csv('sampled_data/generated_sonia_VJL_adaptive.csv')
+# translate to adaptive
+olga2adaptive(pd.read_csv('sampled_data/generated_sonia_leftright.csv')).to_csv('sampled_data/generated_sonia_leftright_adaptive.csv')
